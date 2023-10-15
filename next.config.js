e17d6b351809e37df88ba7b100927bbc7213e1c4
Letho <3763819+LaceLetho@users.noreply.github.com>
2023-10-15 22:27:09 +0800

    debug: cors

--- next.config.js
+++ next.config.js
@@ -8,10 +8,7 @@ module.exports = {
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value:
-              process.env.NODE_ENV === "production"
-                ? "https://aa.tradao.xyz"
-                : "http://localhost:3000",
+            value: "https://aa.tradao.xyz",
           },
           {
             key: "Access-Control-Allow-Methods",
